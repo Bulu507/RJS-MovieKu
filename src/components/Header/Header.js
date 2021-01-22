4dd9a85e8720e0b257943c035dbda5aa049aaaf2
Bulu507 <53290382+Bulu507@users.noreply.github.com>
2021-01-22 23:58:20 +0700

    add filter date

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import './Header.css';
 import { useHistory, Link } from "react-router-dom";
+import { YearPicker } from 'react-dropdown-date';
 
 const Header = (props) => {
     const [searchValue, setSearchValue] = useState('');
+    const [yearValue, setYearValue] = useState('');
     const history = useHistory();
 
     const handleOnSubmit = (e) => {
@@ -11,8 +13,13 @@ const Header = (props) => {
            
 
         if(searchValue){
-            history.push(`/search/${searchValue}`);
+            if(yearValue){
+                history.push(`/search/${searchValue}&year=${yearValue}`);
+            }else{
+                history.push(`/search/${searchValue}`);
+            }
             setSearchValue('');
+            setYearValue('');
         }
 
     }
@@ -21,20 +28,40 @@ const Header = (props) => {
         setSearchValue(e.target.value);
     }
 
+    const date = new Date(props.release_date);
+    const currentYear = date.getFullYear();
+
     return (
         <div className="container-header">
             <Link  to="/" className="logo">
                 <h1>MovieKu</h1>
             </Link>
-            <form onSubmit={handleOnSubmit} className="container-search">
-                <input 
-                    className="search" 
-                    type="text" 
-                    placeholder="Search..." 
-                    value={searchValue}
-                    onChange={handleOnChange}
-                />
-            </form>
+            <div className="searching">
+                    <YearPicker
+                        defaultValue={'select year'}
+                        end={currentYear}                  // default is current year
+                        reverse                     // default is ASCENDING
+                        required={true}             // default is false
+                        value={yearValue}     // mandatory
+                        onChange={(year) => {       // mandatory
+                            setYearValue(year);
+                            console.log(year);
+                        }}
+                        id={'year'}
+                        name={'year'}
+                        classes={'classes'}
+                        optionClasses={'option classes'}
+                    />
+                <form onSubmit={handleOnSubmit} className="container-search">
+                    <input 
+                        className="search" 
+                        type="text" 
+                        placeholder="Search..." 
+                        value={searchValue}
+                        onChange={handleOnChange}
+                    />
+                </form>
+            </div>
         </div>
     )
 }
